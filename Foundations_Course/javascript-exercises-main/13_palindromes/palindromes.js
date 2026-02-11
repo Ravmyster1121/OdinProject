a30a3476ef30a404031c3c6fbcbc9f6686df308d
Ravik Hira <ravik.h@cotiss.com>
2026-02-11 13:13:36 +1300

    Completing palindrome exercise

--- Foundations_Course/javascript-exercises-main/13_palindromes/palindromes.js
+++ Foundations_Course/javascript-exercises-main/13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (rawStr) {
+  let str = rawStr.toLowerCase().replace(/[^a-z0-9]/g, "");
+  let revstr = [];
+  for (let i = str.length; i > 0; i--) {
+    revstr.push(str[i - 1]);
+  }
+  revstr = revstr.join("").toString();
+  console.log(`revstr ${revstr} \nstr    ${str}`);
+  return str == revstr ? true : false;
 };
 
 // Do not edit below this line
