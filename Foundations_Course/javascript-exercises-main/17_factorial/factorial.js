abfbbb181381be015291eedc817e2f49a20d13db
Ravik Hira <ravik.h@cotiss.com>
2026-02-11 17:10:44 +1300

    Completed factorial exercise

--- Foundations_Course/javascript-exercises-main/17_factorial/factorial.js
+++ Foundations_Course/javascript-exercises-main/17_factorial/factorial.js
@@ -1,5 +1,8 @@
-const factorial = function() {
-
+const factorial = function (input) {
+  if (!Number.isInteger(input)) return undefined;
+  if (input < 0) return undefined;
+  if (input === 0) return 1;
+  return input * factorial(input - 1);
 };
 
 // Do not edit below this line
