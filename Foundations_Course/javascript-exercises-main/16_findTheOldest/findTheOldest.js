3137916a8991b6cb4c5fb20ae9bc9d96b6a2c852
Ravik Hira <ravik.h@cotiss.com>
2026-02-11 16:14:01 +1300

    Completed findTheOldest exercise

--- Foundations_Course/javascript-exercises-main/16_findTheOldest/findTheOldest.js
+++ Foundations_Course/javascript-exercises-main/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  let EldestPerson = { name: "", yearOfBirth: 0, yearOfDeath: 0, age: 0 };
+  people.forEach((person) => {
+    if (person.yearOfDeath === undefined) {
+      person.yearOfDeath = new Date().getFullYear();
+    }
+    let age = person.yearOfDeath - person.yearOfBirth;
+    if (age > EldestPerson.age) {
+      EldestPerson = {
+        name: person.name,
+        yearOfBirth: person.yearOfBirth,
+        yearOfDeath: person.yearOfDeath,
+        age: age,
+      };
+    }
+  });
+  return EldestPerson;
 };
 
 // Do not edit below this line
